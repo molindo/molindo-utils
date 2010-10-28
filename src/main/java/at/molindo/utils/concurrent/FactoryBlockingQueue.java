686f3533b757154ac714b416a4153da0982954ff
Stefan Fussenegger <stf+git@molindo.at>
2010-10-28 17:18:34 +0200

    BlockingQueue implementation that has by definition always a size of 1. it creates new elements as needed

---
+++ src/main/java/at/molindo/utils/concurrent/FactoryBlockingQueue.java
@@ -0,0 +1,247 @@
+/**
+ * Copyright 2010 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package at.molindo.utils.concurrent;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public abstract class FactoryBlockingQueue<E> implements BlockingQueue<E> {
+
+	private ArrayBlockingQueue<E> _queue = new ArrayBlockingQueue<E>(1);
+	
+	private final void fill() {
+		if (_queue.size() == 0) {
+			E e = create();
+			if (e == null) {
+				throw new NullPointerException("e");
+			}
+			_queue.add(e);
+		}
+	}
+	
+	/**
+	 * @return a new element, never null
+	 */
+	protected abstract E create();
+	
+	@Override
+	public E remove() {
+		// poll never returns null in this implementation
+		return poll();
+	}
+
+	public E take() throws InterruptedException {
+		// poll never returns null in this implementation
+		return poll();
+	}
+	
+	public E poll(long timeout, TimeUnit unit) throws InterruptedException {
+		// poll never returns null in this implementation
+		return poll();
+	}
+	
+	@Override
+	public E poll() {
+		E e;
+		
+		synchronized (_queue) {
+			fill();
+			e = _queue.poll();
+		}
+		
+		if (e == null) {
+			throw new RuntimeException("queue empty");
+		}
+		return e;
+	}
+
+	public E element() {
+		// peek never returns null in this implementation
+		return peek();
+	}
+
+	public synchronized E peek() {
+		E e;
+		
+		synchronized (_queue) {
+			fill();
+			e = _queue.peek();
+		}
+		
+		if (e == null) {
+			throw new RuntimeException("queue empty");
+		}
+		return e;
+	}
+	
+	/**
+	 * @return always false
+	 */
+	public boolean addAll(Collection<? extends E> c) {
+		return false;
+	}
+
+	/**
+	 * @throws always an {@link IllegalStateException}
+	 */
+	public boolean add(E e) {
+		throw new IllegalStateException("operation not supported");
+	}
+
+	/**
+	 * @return always false
+	 */
+	public boolean offer(E e) {
+		return false;
+	}
+
+	/**
+	 * WARNING: blocks indefinitely as this queue is always full by definition
+	 */
+	public void put(E e) throws InterruptedException {
+		// this is a very bad idea
+		Object mon = new Object();
+		synchronized (mon) {
+			mon.wait();
+		}
+	}
+
+	/**
+	 * blocks until timeout as this queue is always full by definition
+	 */
+	public boolean offer(E e, long timeout, TimeUnit unit)
+			throws InterruptedException {
+		
+		Object mon = new Object();
+		synchronized (mon) {
+			mon.wait(unit.toMillis(timeout));
+		}
+		return false;
+	}
+
+	public int drainTo(Collection<? super E> c) {
+		return drainTo(c, 1);
+	}
+
+	public int drainTo(Collection<? super E> c, int maxElements) {
+		if (c == this) {
+			throw new IllegalArgumentException("cannot drainTo self");
+		}
+		
+		if (maxElements == 0) {
+			return 0;
+		} else {
+			c.add(remove());
+			return 1;
+		}
+	}
+	
+	public boolean containsAll(Collection<?> c) {
+		return _queue.containsAll(c);
+	}
+	
+	public boolean removeAll(Collection<?> c) {
+		return _queue.removeAll(c);
+	}
+
+	public boolean retainAll(Collection<?> c) {
+		synchronized (_queue) {
+			if (_queue.size() == 0) {
+				// pretend to remove yet uncreated object
+				return true;
+			} else {
+				return _queue.retainAll(c);
+			}
+		}
+	}
+
+	public boolean remove(Object o) {
+		return _queue.remove(o);
+	}
+
+	public boolean contains(Object o) {
+		return _queue.contains(o);
+	}
+	
+	public void clear() {
+		_queue.clear();
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public int size() {
+		return 1;
+	}
+
+	public int remainingCapacity() {
+		return 0;
+	}
+
+	public Object[] toArray() {
+		return new Object[] { peek() };
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(T[] a) {
+		if (a.length == 0) {
+            a = (T[])java.lang.reflect.Array.newInstance(
+                    a.getClass().getComponentType(),
+                    1
+                    );
+		}
+		
+		a[0] = (T) peek();
+		
+        if (a.length > 1) {
+            a[1] = null;
+        }
+        
+		return a;
+	}
+
+	public Iterator<E> iterator() {
+		return new Iterator<E>() {
+
+			private E _last;
+
+			@Override
+			public boolean hasNext() {
+				return true;
+			}
+
+			@Override
+			public E next() {
+				return _last = peek();
+			}
+
+			@Override
+			public void remove() {
+				if (_last == null) {
+					throw new IllegalStateException("next() not called or already deleted");
+				} else {
+					FactoryBlockingQueue.this.remove(_last);
+					_last = null;
+				}
+			}
+		};
+	}
+}
---
+++ src/test/java/at/molindo/utils/concurrent/FactoryBlockingQueueTest.java
@@ -0,0 +1,256 @@
+/**
+ * Copyright 2010 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package at.molindo.utils.concurrent;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class FactoryBlockingQueueTest {
+
+	private static FactoryBlockingQueue<Integer> q() {
+		return new FactoryBlockingQueue<Integer>() {
+
+			private int _next = 0;
+
+			@Override
+			protected Integer create() {
+				return _next++;
+			}
+
+		};
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testAdd() {
+		q().add(42);
+	}
+
+	@Test
+	public void testAddAll() {
+		assertFalse(q().addAll(Arrays.asList(42)));
+	}
+
+	@Test
+	public void testClear() {
+		FactoryBlockingQueue<Integer> q = q();
+		Integer i = q.peek();
+		assertTrue(q.contains(i));
+
+		q.clear();
+
+		assertFalse(q.contains(i));
+	}
+
+	@Test
+	public void testContains() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertFalse(q.contains(42));
+
+		Integer i = q.peek();
+		assertTrue(q.contains(i));
+
+		assertEquals(i, q.poll());
+
+		assertFalse(q.contains(i));
+	}
+
+	@Test
+	public void testContainsAll() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertFalse(q.contains(42));
+
+		Integer i = q.peek();
+		assertTrue(q.containsAll(Arrays.asList(i)));
+		assertFalse(q.containsAll(Arrays.asList(i, 42)));
+
+		assertEquals(i, q.poll());
+
+		assertFalse(q.containsAll(Arrays.asList(i)));
+	}
+
+	@Test
+	public void testDrainToCollectionOfQsuperE() {
+		assertEquals(1, q().drainTo(new ArrayList<Integer>()));
+	}
+
+	@Test
+	public void testDrainToCollectionOfQsuperEInt() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertEquals(1, q.drainTo(new ArrayList<Integer>(), 10));
+		assertEquals(0, q.drainTo(new ArrayList<Integer>(), 0));
+	}
+
+	@Test
+	public void testElement() {
+		assertNotNull(q().element());
+	}
+
+	@Test
+	public void testIsEmpty() {
+		assertFalse(q().isEmpty());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIterator() {
+		FactoryBlockingQueue<Integer> q = q();
+		Iterator<Integer> iter = q.iterator();
+		
+		assertTrue(iter.hasNext());
+		
+		Integer i = iter.next();
+		
+		assertTrue(q.contains(i));
+		
+		iter.remove();
+		
+		assertFalse(q.contains(i));
+
+		assertTrue(iter.hasNext());
+		
+		iter.remove();
+	}
+
+	@Test
+	public void testOfferE() {
+		assertFalse(q().offer(42));
+	}
+
+	@Test
+	public void testOfferELongTimeUnit() throws InterruptedException {
+		assertFalse(q().offer(42, 50, TimeUnit.MILLISECONDS));
+	}
+
+	@Test
+	public void testPeek() {
+		FactoryBlockingQueue<Integer> q = q();
+		Integer i = q.peek();
+		assertNotNull(i);
+		assertSame(i, q.peek());
+		assertTrue(q.contains(i));
+	}
+
+	@Test
+	public void testPoll() {
+		FactoryBlockingQueue<Integer> q = q();
+		Integer i = q.poll();
+		assertNotNull(i);
+		assertFalse(q.contains(i));
+		assertFalse(i.equals(q.poll()));
+	}
+
+	@Test
+	public void testPollLongTimeUnit() throws InterruptedException {
+		FactoryBlockingQueue<Integer> q = q();
+		Integer i = q.poll(20, TimeUnit.MILLISECONDS);
+		assertNotNull(i);
+		assertFalse(q.contains(i));
+		assertFalse(i.equals(q.poll(20, TimeUnit.MILLISECONDS)));
+	}
+
+	@Test(expected = InterruptedException.class)
+	public void testPut() throws InterruptedException {
+
+		final Thread t = Thread.currentThread();
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				t.interrupt();
+			}
+		}.start();
+
+		q().put(42);
+	}
+
+	@Test
+	public void testRemainingCapacity() {
+		assertEquals(0, q().remainingCapacity());
+	}
+
+	@Test
+	public void testRemove() {
+		FactoryBlockingQueue<?> q = q();
+		assertNotNull(q.remove());
+	}
+
+	@Test
+	public void testRemoveAll() {
+		FactoryBlockingQueue<Integer> q = q();
+
+		Integer i = q.peek();
+
+		assertTrue(q.contains(i));
+		q.removeAll(Arrays.asList(i));
+		assertFalse(q.contains(i));
+
+	}
+
+	@Test
+	public void testRemoveObject() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertFalse(q.contains(42));
+		Integer i = q.peek();
+		assertTrue(q.contains(i));
+		q.remove(i);
+		assertFalse(q.contains(i));
+	}
+
+	@Test
+	public void testRetainAll() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertTrue(q.retainAll(Arrays.asList(42)));
+
+		Integer i = q.peek();
+		assertFalse(q.retainAll(Arrays.asList(i, 42)));
+		
+		q.remove(i);
+		assertTrue(q.retainAll(Arrays.asList(i, 42)));
+	}
+
+	@Test
+	public void testSize() {
+		assertEquals(1, q().size());
+	}
+
+	@Test
+	public void testTake() throws InterruptedException {
+		FactoryBlockingQueue<?> q = q();
+		assertNotNull(q.take());
+	}
+
+	@Test
+	public void testToArray() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertArrayEquals(new Integer[] {q.peek()}, q.toArray());
+	}
+
+	@Test
+	public void testToArrayTArray() {
+		FactoryBlockingQueue<Integer> q = q();
+		assertArrayEquals(new Integer[] {q.peek(), null, 42}, q.toArray(new Integer[] {42, 42, 42}));
+	}
+
+}
