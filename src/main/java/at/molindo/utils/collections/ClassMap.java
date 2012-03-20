39f5dae6ebad3aa2aea996b6a2f4ba91f302af10
Stefan Fussenegger <stf+git@molindo.at>
2012-03-20 12:01:11 +0100

    ClassMap, see http://bit.ly/GBkZgX

---
+++ src/main/java/at/molindo/utils/collections/ClassMap.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2010 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.molindo.utils.collections;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class ClassMap<V> extends WeakHashMap<Class<?>, V> {
+
+	public ClassMap() {
+		super();
+	}
+
+	public ClassMap(int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	public ClassMap(int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	public ClassMap(Map<? extends Class<?>, ? extends V> m) {
+		super(m);
+	}
+
+	public V find(Class<?> cls) {
+		if (cls == null) {
+			throw new NullPointerException("cls");
+		}
+
+		V v;
+		do {
+			v = get(cls);
+			cls = cls.getSuperclass();
+		} while (v == null && cls != null);
+		return v;
+	}
+
+}
